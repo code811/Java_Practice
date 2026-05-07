68d95a24906444eeda1eda08a0b2a4657c1a9897
code181 <carsonnnghiem@gmail.com>
2026-05-06 22:41:14 -0400

    Start on LeaveRequest
    Set up the basics of the LeaveRequest class

---
+++ Course3/Case Study - Leave Tracking System/src/LeaveRequest.java
@@ -0,0 +1,61 @@
+public class LeaveRequest {
+
+    private final int requestId;
+    private final Employee employee;
+    private String startDate;
+    private String endDate;
+    private String status = "Pending";    // "Pending", "Approved", "Denied"
+    private String reason;
+
+    public LeaveRequest(int requestId, Employee employee, String startDate, String endDate, String reason) {
+        this.requestId = requestId;
+        this.employee = employee;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.reason = reason;
+    }
+
+    public int getRemainingBalance(int requestAmount) {
+        return employee.getLeaveBalance() - requestAmount;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
