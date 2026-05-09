8d26475d7b490f52a7eebbd8ae65405a65bb2d38
code181 <carsonnnghiem@gmail.com>
2026-05-09 15:23:19 -0400

    Move Date.java from LeaveRequest.java
    Did not make sense for the Date class to be grouped with the LeaveRequest class.

---
+++ Course3/Case Study - Leave Tracking System/src/Date.java
@@ -0,0 +1,24 @@
+class Date {
+
+    private int month;
+    private int days;
+    private int year;
+
+    public Date(int month, int days, int year) {
+        this.month = month;
+        this.days = days;
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
--- Course3/Case Study - Leave Tracking System/src/LeaveRequest.java
+++ Course3/Case Study - Leave Tracking System/src/LeaveRequest.java
@@ -110,29 +110,3 @@ class MaternityLeaveRequest extends LeaveRequest {
         return fmlaEligible;
     }
 }
-
-
-class Date {
-
-    private int month;
-    private int days;
-    private int year;
-
-    public Date(int month, int days, int year) {
-        this.month = month;
-        this.days = days;
-        this.year = year;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getDays() {
-        return days;
-    }
-
-    public int getYear() {
-        return year;
-    }
-}
