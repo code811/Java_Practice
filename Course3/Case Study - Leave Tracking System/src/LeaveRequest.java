2d86a94f01e6404e56b7482504a1d3263767c0f9
code181 <carsonnnghiem@gmail.com>
2026-05-09 14:13:47 -0400

    Add SickLeaveRequest
    Extends LeaveRequest and asks for an additional parameter: medicalCertificateProvided.

--- Course3/Case Study - Leave Tracking System/src/LeaveRequest.java
+++ Course3/Case Study - Leave Tracking System/src/LeaveRequest.java
@@ -1,4 +1,4 @@
-public class LeaveRequest {
+class LeaveRequest {
 
     private final int requestId;
     private final Employee employee;
@@ -57,5 +57,22 @@ public class LeaveRequest {
 
     public void setReason(String reason) {
         this.reason = reason;
+    }
+}
+
+
+class SickLeaveRequest extends LeaveRequest {
+
+    private boolean medicalCertificateProvided;
+
+    public SickLeaveRequest(int requestId, Employee employee,
+                            String startDate, String endDate,
+                            boolean medicalCertificateProvided) {
+        super(requestId, employee, startDate, endDate, "Sick Leave");
+        this.medicalCertificateProvided = medicalCertificateProvided;
+    }
+
+    public boolean isMedicalCertificateProvided() {
+        return medicalCertificateProvided;
     }
 }
